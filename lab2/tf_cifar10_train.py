ac0e74f2ba2ba553d4d7782bbbfb3c05c633a878
Kristijan <kristijan.bartol@gmail.com>
2017-11-13 22:14:44 +0100

    Initialized CIFAR10 assignment

---
+++ lab2/tf_cifar10_train.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+
+# conv(16,5) -> relu() -> pool(3,2) -> conv(32,5) -> relu() -> pool(3,2) -> fc(256) -> relu() -> fc(128) -> relu() -> fc(10)
+
+
+def evaluate():
+    pass
